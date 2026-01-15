f0aebf1a9487d623d7b555e745d02e7753cf278b
Michael Liu <hello@convoliution.com>
2026-01-15 12:00:33 -0800

    Merge git.nix back into home.nix

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -5,11 +5,6 @@ let
   sshPath = "${config.home.homeDirectory}/.ssh/id_ed25519";
 in
 {
-  imports = [
-    ./firefox.nix
-    (import ./git.nix { inherit sshPath email; })
-  ];
-
   programs.home-manager.enable = true;
   news.display = "silent";
   home = {
@@ -58,6 +53,82 @@ in
       "Library/Application Support/Firefox/Profiles/nightly/user.js".source =
         config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/user.js";
     };
+  };
+
+  programs.git = {
+    enable = true;
+    aliases = {
+      alog = "log --graph --all --format=format:'%C(bold yellow)%h%C(reset) - %C(bold blue)%ar%C(reset)%C(auto)%d%C(reset)%n%w(72,10,10)%C(white)%s%C(reset)%n%C(dim white)%an%C(reset)'";
+    };
+    userEmail = email;
+    userName = "Michael Liu";
+    signing = {
+      key = "${sshPath}.pub";
+      signByDefault = true;
+    };
+    extraConfig = {
+      advice.detachedHead = "false";
+      commit.verbose = "true";
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = "true";
+        renames = "copies";
+      };
+      fetch = {
+        all = "true";
+        prune = "true";
+        pruneTags = "true";
+      };
+      gpg.format = "ssh";
+      init.defaultBranch = "main";
+      merge.conflictstyle = "zdiff3";
+      pull.rebase = "true";
+      push.autoSetupRemote = "true";
+      rebase.updateRefs = "true";
+      tag.sort = "version:refname";
+    };
+    ignores = [
+      # compiled source
+      "*.com"
+      "*.class"
+      "*.dll"
+      "*.exe"
+      "*.o"
+      "*.pyc"
+      "*.so"
+
+      # packages
+      "*.7z"
+      "*.dmg"
+      "*.gz"
+      "*.iso"
+      "*.jar"
+      "*.rar"
+      "*.tar"
+      "*.zip"
+
+      # logs and databases
+      "*.log"
+      "*.sql"
+      "*.sqlite"
+
+      # caches
+      ".sass-cache"
+      "__pycache__"
+
+      # OS generated files
+      ".DS_Store"
+      ".DS_Store?"
+      "._*"
+      ".Spotlight-V100"
+      ".Trashes"
+      "ehthumbs.db"
+      "Thumbs.db"
+
+      # Jupyter Notebook checkpoints
+      ".ipynb_checkpoints"
+    ];
   };
 
   programs.ssh = {
