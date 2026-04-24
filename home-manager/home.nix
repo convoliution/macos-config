9b3c84546e1bc1d3f2eb6823f9141729831a665d
Michael Liu <hello@convoliution.com>
2026-04-23 17:39:08 -0700

    Convert script into shell command

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -48,6 +48,42 @@ in
           home-manager switch --flake path:"$HOME"/.config/home-manager
         '';
       })
+
+      (writeShellApplication {
+        name = "medial";
+        text = ''
+          urls=$(mktemp)
+          vim "$urls"
+
+          if [[ ! -s "$urls" ]]; then
+              rm "$urls"
+              echo "No URLs entered. Exiting."
+              exit 0
+          fi
+
+          mkdir -p .downloaded
+          yt-dlp -f "bestvideo+bestaudio/best" \
+              -o ".downloaded/%(uploader)s-%(id)s.%(ext)s" \
+              --cookies-from-browser firefox \
+              -a "$urls"
+
+          outdir=$(date +%Y-%m-%d)
+          mkdir -p "$outdir"
+          for f in .downloaded/*; do
+              mime=$(file --mime-type -b "$f")
+              if [[ "$mime" == video/* ]]; then
+                  basename_f=$(basename "$f")
+                  name="''${basename_f%.*}"
+                  ffmpeg -i "$f" -vcodec libx264 -pix_fmt yuv420p -an "''${outdir}/''${name}.mp4"
+              elif [[ "$mime" == image/* ]]; then
+                  cp "$f" "''${outdir}/$(basename "$f")"
+              fi
+          done
+
+          rm -r .downloaded
+          rm "$urls"
+        '';
+      })
     ];
 
     file = {
